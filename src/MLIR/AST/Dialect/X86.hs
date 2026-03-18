975986fd8756a454679034de159acf76f20e03ce
Jacques Pienaar <jpienaar@google.com>
2026-03-18 21:23:52 +0200

    Misc fixes (#64)
    * Add UB dialect to cabal exposed modules
    
    * Update doc workflow to run after CI.
    
    * Rename x86vector dialect.

--- src/MLIR/AST/Dialect/X86.hs
+++ src/MLIR/AST/Dialect/X86.hs
@@ -12,8 +12,8 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
-module MLIR.AST.Dialect.X86Vector
-  ( module MLIR.AST.Dialect.Generated.X86Vector
+module MLIR.AST.Dialect.X86
+  ( module MLIR.AST.Dialect.Generated.X86
   ) where
 
-import MLIR.AST.Dialect.Generated.X86Vector
+import MLIR.AST.Dialect.Generated.X86
